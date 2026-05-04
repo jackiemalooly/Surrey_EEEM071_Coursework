6a78c05aa7e6033b33c0cf52e275039ef4b843e7
jackiemalooly <jackie.malooly@renaissance.com>
2026-05-04 17:01:17 +0100

    Ran analysis for data augmentation techniques.

--- test.sh
+++ test.sh
@@ -3,9 +3,9 @@
 STUDENT_ID=6861173 STUDENT_NAME="Jackie Malooly" python main.py \
 -s veri \
 -t veri \
--a googlenet \
+-a resnet50 \
 --evaluate \
 --visualize-ranks \
---resume logs/googlenet-veri/model.pth.tar-10 \
+--resume logs/resnet50-veri/model.pth.tar-10 \
 --root /user/HS402/jm02999/Surrey_EEEM071_Coursework \
---save-dir logs/googlenet-veri \
+--save-dir logs/resnet50-veri \
