483f3b1b572f2764289741a7c1b36d8ba9155c39
jackiemalooly <jackie.malooly@renaissance.com>
2026-05-05 09:28:07 +0100

    Ran a few more tests on optimizers

--- train.sh
+++ train.sh
@@ -7,12 +7,12 @@ STUDENT_ID=6861173 STUDENT_NAME="Jackie Malooly" python main.py \
 --root /user/HS402/jm02999/Surrey_EEEM071_Coursework \
 --height 224 \
 --width 224 \
---optim amsgrad \
---lr 0.08 \
+--lr 0.00008 \
 --max-epoch 10 \
 --stepsize 20 40 \
---train-batch-size 128 \
+--train-batch-size 16 \
 --test-batch-size 100 \
 --color-jitter \
 --random-erase \
---save-dir logs/googlenet-color-lr-batch-128-veri \
+--optim adam \
+--save-dir logs/googlenet-color-lr-batch-16-adam-veri \
